060b59b3fc6e20a2611ad82af07405f91f1ff6ac
Davies Esther <daviesesther74@gmail.com>
2020-11-24 02:20:27 +0100

    creating files and folder

---
+++ script.js
@@ -0,0 +1,29 @@
+function signUp() {
+    confirm('Are you sure you want to submit this form?');
+         let user = {};
+   user.name;
+ }
+
+
+function submitInfo(){
+    var firstName= '';
+    var lastName= '';
+    var email= '';
+    var password= '';
+
+    firstName = document.getElementById('firstName').value;
+    lastName = document.getElementById('lastName').value;
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+    console. log('first Name:' + firstName);
+    console. log('Last Name:' + lastName);
+    console. log('Email:' + email);
+    console. log('password:' + password);
+
+    if (firstName ==''|| lastName == '' || email =='' || password ==''){
+        alert('Input First Name');
+ }else{  
+  alert('form has been submitted');
+}
+
+}
